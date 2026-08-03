0159bbf25863fb098696d360521c4c6417b13104
grodriguezal <119162814+grodriguezal@users.noreply.github.com>
2026-08-03 17:28:24 -0400

    Use definitive character artwork in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import "./character.css";
 
 const nunito = Nunito_Sans({ variable: "--font-brand", subsets: ["latin"] });
 
